c27cab42fe124496d91e898f2dbc5de0147850e1
devpunumalli <devpunumalli@gmail.com>
2020-12-21 12:26:21 -0500

    Issue#3creation of controller is done

---
+++ src/main/java/com/dev/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,32 @@
+package com.dev.msscbeerservice.web.controller;
+
+
+import com.dev.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping(("/api/v1/beer"))
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId){
+
+        return new ResponseEntity<BeerDto>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveBeer(@RequestBody BeerDto beerDto){
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity updateBeer(@RequestBody BeerDto beerDto){
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
+}
--- src/main/java/com/dev/msscbeerservice/web/model/BeerStyleEnum.java
+++ src/main/java/com/dev/msscbeerservice/web/model/BeerStyleEnum.java
@@ -1,4 +1,8 @@
 package com.dev.msscbeerservice.web.model;
 
+/**
+ *
+ */
 public enum BeerStyleEnum {
+    LAGER,PILSNER,STOUT,GOSE,PORTER,ALE,WHEAT,IPA,PALE_ALE,SAISON
 }
