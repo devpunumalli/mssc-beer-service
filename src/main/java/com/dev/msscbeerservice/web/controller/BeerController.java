3cf505fd115c6f1a60aa291391d4d5dec1fb4f0a
devpunumalli <devpunumalli@gmail.com>
2020-12-26 12:27:35 -0500

    modification controller by integrating with services and repository, closes #6

--- src/main/java/com/dev/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/dev/msscbeerservice/web/controller/BeerController.java
@@ -1,7 +1,10 @@
 package com.dev.msscbeerservice.web.controller;
 
 
+import com.dev.msscbeerservice.services.BeerService;
+import com.dev.msscbeerservice.web.mapper.BeerMapper;
 import com.dev.msscbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,23 +12,30 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.UUID;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping(("/api/v1/beer"))
 public class BeerController {
 
+    private final BeerService beerService;
+
+
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId){
-
-        return new ResponseEntity<BeerDto>(BeerDto.builder().build(), HttpStatus.OK);
+        BeerDto dto = beerService.getBeerById( beerId);
+        return new ResponseEntity<BeerDto>(dto, HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity saveBeer(@Valid @RequestBody BeerDto beerDto){
+
+        beerService.saveBeer(beerDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
     public ResponseEntity updateBeer(@PathVariable("beerId") UUID beerId, @Valid @RequestBody BeerDto beerDto){
+        beerService.updateBeer(beerId,beerDto);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
---
+++ src/main/java/com/dev/msscbeerservice/web/controller/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.dev.msscbeerservice.web.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+
+public class NotFoundException extends RuntimeException{
+}
