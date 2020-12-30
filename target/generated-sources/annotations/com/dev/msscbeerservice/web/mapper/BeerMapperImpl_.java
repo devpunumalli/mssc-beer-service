3d037cbcbdaf0eadaed1ef343d9abdafa13b2105
devpunumalli <devpunumalli@gmail.com>
2020-12-29 20:56:21 -0500

    update the BOM in maven central #17

--- target/generated-sources/annotations/com/dev/msscbeerservice/web/mapper/BeerMapperImpl_.java
+++ target/generated-sources/annotations/com/dev/msscbeerservice/web/mapper/BeerMapperImpl_.java
@@ -12,8 +12,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-12-29T20:10:18-0500",
-    comments = "version: 1.4.1.Final, compiler: javac, environment: Java 11.0.9.1 (Amazon.com Inc.)"
+    date = "2020-12-29T20:50:38-0500",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.9.1 (Amazon.com Inc.)"
 )
 @Component
 @Qualifier("delegate")
